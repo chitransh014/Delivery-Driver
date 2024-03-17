095b09c278dbaa9b73141eadc32e072917f0ff59
chitransh014 <145492599+chitransh014@users.noreply.github.com>
2024-03-17 09:41:37 +0530

    project complete

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,20 +4,35 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField]float SteerSpeed = 0.1f;
-    [SerializeField]float moveSpeed = 0.01f;
+    [SerializeField] float SteerSpeed = 1f;
+    [SerializeField] float moveSpeed = 20f;
+     [SerializeField] float slowSpeed = 15f;
+      [SerializeField] float boostSpeed = 30f;
 
-    void Start()
-    {
-        
-    }
 
-    void Update()
+       void Update()
     {
         float steerAmount = Input.GetAxis("Horizontal") * SteerSpeed * Time.deltaTime;
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
-        transform.Rotate(0, 0, -steerAmount );
+        transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+       
+        
+           // Debug.Log("steerAmount:" + steerAmount + ", move:" + moveAmount);
+        
 
+    }
+     void OnCollisionEnter2D(Collision2D other)
+      {
+        moveSpeed = slowSpeed;
+    }
+     void OnTriggerEnter2D(Collider2D other)
+      {
+        if(other.tag == "Boost")
+        {
+            Debug.Log("you are boosting, man!");
+            moveSpeed = boostSpeed;
+
+        }
     }
 }
--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -6,8 +6,8 @@ public class FollowCamera : MonoBehaviour
 {
     [SerializeField] GameObject thingToFollow;
     // this things position (camera) should be the same as the car's position
-    void Update()
+    void LateUpdate()
     {
-      //  transform.position =
+        transform.position = thingToFollow.transform.position + new Vector3 (-13,1.7f,-929.5f);
     }
 }
